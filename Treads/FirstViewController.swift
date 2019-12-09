1ac92953bd105b4bad2a5d077bf3b908a45ad44b
Thai Thi Tu Trinh <ttttrinh132@gmail.com>
2019-12-09 23:19:33 +0700

    init project

---
+++ Treads/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Treads
+//
+//  Created by Trinh Thai on 12/9/19.
+//  Copyright © 2019 Trinh Thai. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
